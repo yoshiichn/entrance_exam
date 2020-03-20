881395200cfd74270a0443c69ecdbf1cab247ae4
yoshiichn <62328713+yoshiichn@users.noreply.github.com>
2020-03-20 23:52:56 +0900

    Add exam2 source code
    Add exam code and test code.

---
+++ exam2.py
@@ -0,0 +1,24 @@
+import exam1
+
+
+class Classification:
+    def __init__(self, classification_name, subject_array):
+        self.classification_name = classification_name
+        self.subject_array = subject_array
+
+    def output_classification_data(self):
+        result = self._calc_amount()
+        print(self.classification_name, result[0], result[1])
+
+    def output_classification_calc_data(self):
+        result = self._calc_amount()
+        total_amount = result[1] - result[0]
+        print(self.classification_name, total_amount)
+
+    def _calc_amount(self):
+        total_previous_term_amount = 0
+        total_current_period_amount = 0
+        for subject in self.subject_array:
+            total_previous_term_amount += subject.previous_term_amount
+            total_current_period_amount += subject.current_period_amount
+        return total_previous_term_amount, total_current_period_amount
--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import exam1
+import exam2
 
 print("--- exam1 test ---")
 cash_and_deposit = exam1.Subject('現金及び預金', 4564255, 5160507)
 cash_and_deposit.output_account_data()
 cash_and_deposit.output_account_calc_data()
+
+print("\n--- exam2 test ---")
+notes_and_accounts_receivable = exam1.Subject('受け取り手形及び売掛金', 2013110, 2525653)
+
+subject_list = [cash_and_deposit, notes_and_accounts_receivable]
+current_assets = exam2.Classification('流動資産', subject_list)
+current_assets.output_classification_data()
